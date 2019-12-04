b17ddec4dd091f7061510ae4751e5b1672e1ae86
uuparupa <uparupa@uuparupa-no-MacBook-Air.local>
2019-12-04 16:44:35 +0900

    検索レスポンスの改善

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -4,6 +4,7 @@ import { NavController,LoadingController } from 'ionic-angular';
 import { ViewPage } from '../view/view';
 import { HttpClient } from '@angular/common/http';
 import { jsonpCallbackContext } from '@angular/common/http/src/module';
+import { REACTIVE_DRIVEN_DIRECTIVES } from '@angular/forms/src/directives';
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
@@ -11,10 +12,8 @@ import { jsonpCallbackContext } from '@angular/common/http/src/module';
 export class AboutPage {
   spn: any;
   SN: any;
+  timerId: any;
   si = {
-    'width': '80px',
-    'height': '80px',
-    'color':'#EFF4FF'
   }
   pi = {
     'color':'pink'
@@ -22,9 +21,10 @@ export class AboutPage {
   kiji: Object;
   constructor(private http: HttpClient,public navCtrl: NavController,public loadingController: LoadingController) {
   }
-  onKey(ev) { 
+  onKey(ev,n) { 
     console.log(ev);
-    this.gech(ev);
+    this.gech(ev, n);
+    console.log("N is ", n);
   }
   click(jsons) {
     console.log(jsons);
@@ -46,21 +46,45 @@ export class AboutPage {
 //      refresher.complete();
 //    }
 //  }
-  async gech(TX) {
-    console.log("gech is RUN");
+  NDE(TX) { 
+
     try {
-      this.spn = '<ion-spinner [ngStyle]="si"></ion-spinner>';
-      const res =  await this.http.get("https://qiita.com/api/v2/items?per_page=100&query=" + TX)
+      this.timerId = setTimeout(() => this.RE(TX), 1000);
+    } catch (err) {
+      this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2><p>' + err["status"] + err["error"]["message"] + '.</p>';
+      console.log("大変申し訳ございませんが、エラーが発生しました。アプリを閉じて再度お試しください。");
+      console.log(err);
+    }
+
+  }
+  async RE(TX) {
+    try {
+      this.spn = '<div padding><ion-spinner name="dots"></ion-spinner></div><p>読み込み中...</p>';
+      setTimeout(() => this.spn='<ion-icon style="font-size: 80px;" name="search"></ion-icon><p>まだ何も検索していないか、検索結果が見つかりません。</p>', 5000);
+      const res = await this.http.get("https://qiita.com/api/v2/items?per_page=100&query=" + TX)
         .toPromise();
       this.SN = TX;
       console.log(res);
       //    NE = res["0"]["title"];
+    //  if (res["length"] = 0) { 
+    //    this.spn = '<p>検索結果はありません。</p>';
+    //  }
       this.kiji = res;
-    } catch (err) { 
-
-      this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2><p>' + err["status"] +err["error"]["message"] + '.</p>';
+    }catch (err) {
+      this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2><p>' + err["status"] + err["error"]["message"] + '.</p>';
       console.log("大変申し訳ございませんが、エラーが発生しました。アプリを閉じて再度お試しください。");
       console.log(err);
+    }
+}
+  async gech(TX, m) {
+    
+    console.log("gech is RUN");
+    if (m) {
+      console.log(this.timerId);
+      clearTimeout(this.timerId);
+      console.log(this.timerId);
+    } else {
+      this.NDE(TX);
     }
   }
   //constructor(public navCtrl: NavController) {
