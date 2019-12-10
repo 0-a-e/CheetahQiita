f51e21bdf13ee4ec89d2f7409aeadedcb00d574b
orangelinux <verneemix2.vlv@gmail.com>
2019-12-10 20:51:48 +0900

    ホームで無限スクロールが可能に

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,8 +9,11 @@ import { PopPage } from '../pop/pop';
 })
 export class HomePage {
   kiji: Object;
+  RC: any;
+  kiji2: Object;
   count: any;
   plus: any;
+  RCS: any;
   Fsave: any;
   spn: any;
   constructor(public popoverController: PopoverController,public navCtrl: NavController, private http: HttpClient) {
@@ -20,8 +23,8 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
+    this.count = 2;
     try {
-      this.count = 1;
       let headers = new HttpHeaders();
       headers = headers.set('Content-Type', 'application/json; charset=utf-8');
       const res = this.http.get("https://qiita.com/api/v2/items?per_page=100")
@@ -45,6 +48,7 @@ export class HomePage {
     }
   }
   async gs(refresher) {
+    const list = document.getElementById('list');
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     console.log("ge is RUN");
@@ -56,21 +60,49 @@ export class HomePage {
       this.count++;
       console.log(this.count);
       //    NE = res["0"]["title"];
-      var ret = Object.assign(this.kiji, res);
-      console.log("KIJI IS ");
-      console.log(this.kiji);
-      console.log("RET IS");
-      console.log(ret);
-      this.kiji = ret;
+      var ret = res;
+      this.kiji2 = ret;
+      this.RCS = "";
+      for (this.RC = 0; 100 > this.RC; this.RC++) {
+        var el = document.createElement('div');
+        console.log(this.RC);
+        var NR = ret[this.RC];
+        //  this.RCS = NR.title;
+        console.log(NR);
+        el.setAttribute('id', NR.id);
+        el.setAttribute('class', 'LIS');
+        el.innerHTML = `<hr><ion-item class="item item-block item-ios" _ngcontent-c1 ><ion-label class="label label-ios label-md" _ngcontent-c1>${NR.title}</ion-label></ion-item>`;
+        list.appendChild(el);
+      }
+      const targets = document.getElementsByClassName('LIS');
+for(let i = 0; i < targets.length; i++){
+  targets[i].addEventListener('click', () => {
+    console.log(i + `をクリックしました`);
+    var id = targets[i].getAttribute("id");
+    console.log(id);
+    const res = this.http.get("https://qiita.com/api/v2/items/" + id)
+    .subscribe(res => {
+      console.log(res);
+      this.navCtrl.push(ViewPage, { "jsons": res });
+     // this.kiji = res;
+    }, error => {
+    //  this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2>';
+    });
+    
+}, false);
+  
+}
+
       refresher.complete();
     } catch (err) { 
-      this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2><p>' + err["status"] +err["error"]["message"] + '.</p>';
+      //this.spn = '<h2 [ngStyle]="pi">エラーが発生しました。しばらく時間をおいて再度お試しください。</h2><p>' + err["status"] +err["error"]["message"] + '.</p>';
       console.log("大変申し訳ございませんが、エラーが発生しました。アプリを閉じて再度お試しください。");
       console.log(err);
       refresher.complete();
     }
   }
   async ge(refresher) {
+    this.RC = 0;
     let headers = new HttpHeaders();
     headers = headers.set('Content-Type', 'application/json; charset=utf-8');
     console.log("ge is RUN");
